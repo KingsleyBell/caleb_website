41e32f3f1a76d870a3bfeeb3f77ee257d37c7d9d
Luke Bell <lukekingsleybell@gmail.com>
2019-10-27 17:38:42 +0000

    Rich text editor.

---
+++ app/static/js/edit_section.js
@@ -0,0 +1,21 @@
+var quill, sectionText;
+
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes[0].nodeValue;
+}
+
+$(document).ready(function () {
+    quill = new Quill('#snow-container', {
+        placeholder: "test",
+        theme: "snow"
+    });
+    quill.clipboard.dangerouslyPasteHTML(htmlDecode(sectionText));
+
+    $("#section-form").on("submit", function () {
+        var myEditor = document.querySelector("#snow-container");
+        var html = myEditor.children[0].innerHTML;
+        $("#text").val(html);
+    });
+});
