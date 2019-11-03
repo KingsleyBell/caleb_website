ac73e248bb797a363b9354f035d48a6546744b68
Luke Bell <lukekingsleybell@gmail.com>
2019-11-03 13:40:51 +0000

    Fix section allocation for upload and edit.

--- app/app.py
+++ app/app.py
@@ -109,7 +109,7 @@ def edit_image(section_id, image_id):
         display_width = request.form.get('display_width')
         align = request.form.get('align')
 
-        db_section = [s for s in db if s['name'] == section_name][0]
+        db_section = [s for s in db if s['id'] == section_name][0]
         db_section['images'].append(image)
         section['images'].remove(image)
 
@@ -127,11 +127,11 @@ def edit_image(section_id, image_id):
 
         return redirect(url_for('sections'))
     else:
-        sections = [section['name'] for section in db]
+        sections = [{"name": section["name"], "id": section['id']} for section in db]
         return render_template(
             'edit_image.html',
             image=image,
-            section=section['name'],
+            section=section['id'],
             sections=sections
         )
 
