f6be40b321535a0950b6dd2eac931b6b74ed7148
Luke Bell <lukekingsleybell@gmail.com>
2019-11-03 13:10:56 +0000

    about html and js.

---
+++ app/static/js/about.js
@@ -0,0 +1,22 @@
+var quill, aboutText;
+
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes[0].nodeValue;
+}
+
+$(document).ready(function () {
+    quill = new Quill('#snow-container', {
+        placeholder: "test",
+        theme: "snow"
+    });
+    console.log(aboutText);
+    quill.clipboard.dangerouslyPasteHTML(htmlDecode(aboutText));
+
+    $("#about-form").on("submit", function () {
+        var myEditor = document.querySelector("#snow-container");
+        var html = myEditor.children[0].innerHTML;
+        $("#text").val(html);
+    });
+});
